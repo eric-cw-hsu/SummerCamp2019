f43c446250472b7e611c537447d7d60c7d197ddd
unknown <eric19981224@gmail.com>
2019-07-02 22:28:03 +0800

    add course4

---
+++ Course4/workshop/workshop/Models/BookSearch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Mvc;
+
+namespace workshop.Models
+{
+    public class BookSearch
+    {
+        [DisplayName("書名")]
+        public string BookName { get; set; }
+        [DisplayName("圖書類別")]
+        public string BookCategory { get; set; }
+        [DisplayName("借閱人")]
+        public string LendName { get; set; }
+        [DisplayName("借閱狀態")]
+        public string BookStatus { get; set; }
+    }
+}
